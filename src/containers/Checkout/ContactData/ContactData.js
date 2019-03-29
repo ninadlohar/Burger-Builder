0bca9e43906b6408d67591b81ee2aff74e82bb25
ninadlohar <ninad.lohar94@gmail.com>
2019-03-29 15:52:34 +0530

    added advance input validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -150,6 +150,8 @@ class ContactData extends React.Component {
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
               value={formElement.config.value}
+              invalid={!formElement.config.valid}
+              shouldValidate={formElement.config.validation}
               changed={event => this.inputChangedHandler(event, formElement.id)}
             />
           );
